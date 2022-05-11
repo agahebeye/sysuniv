cce57933016cf5d2fa264c8928000cf8a09b604b
agahebeye <agahebeye@gmail.com>
2022-05-11 09:39:55 +0200

    add vite scripts to app view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Scripts -->
-        @routes
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    @routes
+    @inertiaHead
+    <script type="module" src="http://localhost:3000/@vite/client"></script>
+    <script type="module" src="http://localhost:3000/resources/js/main.ts"></script>
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+</body>
+
 </html>
