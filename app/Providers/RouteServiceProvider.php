4e8fc7d5dfbfc3df1af14f1e4266139a017f6182
agahebeye <agahebeye@gmail.com>
2022-05-11 09:18:18 +0200

    change route service provider home to dashboard

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashroab';
 
     /**
      * Define your route model bindings, pattern filters, etc.
