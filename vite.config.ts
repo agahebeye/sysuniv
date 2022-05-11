e8266f7d6ca432ef41e20083b87c72c904625fff
agahebeye <agahebeye@gmail.com>
2022-05-11 09:35:30 +0200

    setup vite config

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig(({command}) => ({
+    base: command === 'serve' ? '': '/build/',
+    build: {
+        manifest: true,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: 'resources/js/main.ts'
+        }
+    },
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, 'resources')
+        }
+    },
+    plugins: [vue()]
+}))
