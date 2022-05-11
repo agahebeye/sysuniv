ff9f0066376ca3075a5f15dc3e3eedb31341bf78
agahebeye <agahebeye@gmail.com>
2022-05-11 13:29:59 +0200

    test login controller

--- tests/Feature/Http/Controllers/Auth/AuthenticatedSessionControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/AuthenticatedSessionControllerTest.php
@@ -1,13 +1,25 @@
 <?php
 
-use function Pest\Laravel\get;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+
+use function Pest\Laravel\{get, post};
+use Inertia\Testing\AssertableInertia as Assert;
 
 get('/login')->assertOk();
 
 it('sees a login page', function () {
-    $this->get('/login')->assertInertia(
-        fn ($page) =>
+    get('/login')->assertInertia(
+        fn (Assert $page) =>
         $page->component('auth/login')
             ->has('status', null)
     );
+});
+
+it('logs in users', function () {
+    $user = User::factory()->createQuietly(['password' => 'password']);
+
+    $response = post('/login', ['email' => $user->email, 'password' => 'password']);
+    $this->assertAuthenticated();
+    $response->assertRedirect(RouteServiceProvider::HOME);
 });
