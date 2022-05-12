84b1a795c034c6717af86ce35f13981293d4db58
agahebeye <agahebeye@gmail.com>
2022-05-12 08:30:20 +0200

    add university model

---
+++ database/factories/UniversityFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\University>
+ */
+class UniversityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nom' => $this->faker->sentence(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => 'secretsecret',
+            'nif' => Str::random(8),
+            'siteweb' => $this->faker->domainWord(),
+            'addresse' => $this->faker->address()
+        ];
+    }
+}
