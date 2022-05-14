af6b414c6f5b284720e6fb839bb28e4f614f78b1
agahebeye <agahebeye@gmail.com>
2022-05-14 10:49:34 +0200

    add store university login test

--- tests/Feature/Http/Controllers/Auth/AuthenticatedUniversityControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/AuthenticatedUniversityControllerTest.php
@@ -1,10 +1,14 @@
 <?php
 
-use function Pest\Laravel\get;
+use function Pest\Laravel\{get, post};
 
-it('can create university login', function() {
+it('can create university login', function () {
     get(route('universities.login'))
-    ->assertOk()
-    ->assertInertia(fn($page) => $page->component('universities/auth/login'));
-    ;
+        ->assertOk()
+        ->assertInertia(fn ($page) => $page->component('universities/auth/login'));;
+});
+
+it('can store university login', function () {
+    $response = post(route('universities.login'));
+    dd($response->json());
 });
