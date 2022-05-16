21be2de9d04a3275fdf976d661e09238d4ad469a
agahebeye <agahebeye@gmail.com>
2022-05-16 12:13:09 +0200

    add soft-deletion to faculties

--- database/migrations/2022_05_16_084705_create_faculties_table.php
+++ database/migrations/2022_05_16_084705_create_faculties_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('faculties', function (Blueprint $table) {
             $table->id();
             $table->string('nom')->unique();
+            $table->softDeletes();
             $table->timestamps();
         });
 
