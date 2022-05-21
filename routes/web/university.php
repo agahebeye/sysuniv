242db5ff8e2bf525b73bdd0ab4be84b225492e90
agahebeye <agahebeye@gmail.com>
2022-05-21 17:21:15 +0200

    wip

--- routes/web/university.php
+++ routes/web/university.php
@@ -10,14 +10,12 @@ use App\Http\Controllers\University\UniversityInstituteController;
 Route::prefix('universities')
     ->middleware(['auth'])
     ->group(function () {
-        Route::get('/', [UniversityController::class, 'index'])->name('universities.index');
-        Route::get('/create', [UniversityController::class, 'create'])->name('universities.create');
-        Route::post('/store', [UniversityController::class, 'store'])->name('universities.store');
-        Route::get('/{university}', [UniversityController::class, 'show'])->name('universities.show');
-        Route::get('/{university}/edit', [UniversityController::class, 'edit'])->name('universities.edit');
-        Route::put('/{university}/update', [UniversityController::class, 'update'])->name('universities.update');
-        Route::delete('/{university}/destroy', [UniversityController::class, 'destroy'])->name('universities.destroy');
-        Route::get('/{university}/students', [UniversityStudentController::class, '__invoke'])->name('universities.students');
-        Route::get('/{university}/faculties', [UniversityFacultyController::class, '__invoke'])->name('universities.faculties');
-        Route::get('/{university}/institutes', [UniversityInstituteController::class, '__invoke'])->name('universities.institutes');
+        Route::middleware(['role:admin,employee'])->group(function () {
+            Route::get('/', [UniversityController::class, 'index'])->name('universities.index');
+            Route::get('/create', [UniversityController::class, 'create'])->name('universities.create');
+            Route::post('/store', [UniversityController::class, 'store'])->name('universities.store');
+            Route::get('/{university}/edit', [UniversityController::class, 'edit'])->name('universities.edit');
+            Route::put('/{university}/update', [UniversityController::class, 'update'])->name('universities.update');
+            Route::delete('/{university}/destroy', [UniversityController::class, 'destroy'])->name('universities.destroy');
+        });
     });
