444e32a5325c3e37a6c4d5621ed5728cd825ab61
agahebeye <agahebeye@gmail.com>
2022-05-26 09:05:25 +0200

    add login form

--- resources/js/pages/auth/login.vue
+++ resources/js/pages/auth/login.vue
@@ -1,5 +1,40 @@
+<script lang="ts" setup>
+import { Head, useForm } from '@inertiajs/inertia-vue3';
+
+const form = useForm({
+    email: '',
+    password: ''
+});
+
+function login() {
+    console.log(form);
+}
+</script>
+
 <template>
+
+    <Head>
+        <title>Login - Sysuniv</title>
+    </Head>
     <div>
         <h1>login</h1>
+
+        <div class="errors" v-if="form.hasErrors">
+            {{ form.errors}}
+        </div>
+
+        <form @submit.prevent="form.post('/login')">
+            <div>
+                <label for="email">email</label>
+                <input type="email" id="email" v-model="form.email" autocomplete="off" autofocus required>
+            </div>
+
+            <div>
+                <label for="password">password</label>
+                <input type="password" minlength="8" id="password" v-model="form.password" autocomplete="off" autofocus required>
+            </div>
+
+            <button>Login</button>
+        </form>
     </div>
 </template>
