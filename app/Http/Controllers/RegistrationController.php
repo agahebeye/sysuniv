9c0c9c458cb8ea10838ce87e192d9e590e163cf8
agahebeye <agahebeye@gmail.com>
2022-06-09 10:14:00 +0200

    wip

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -5,15 +5,10 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use App\Models\Faculty;
 use App\Models\Student;
-use App\Enums\LevelType;
 use App\Models\Institute;
-use App\Enums\ResultStatus;
-use App\Models\Registration;
-use Illuminate\Validation\Rules\Enum;
 use App\Providers\RouteServiceProvider;
-use App\Actions\CreateRegistrationAction;
-use Illuminate\Validation\ValidationException;
 use App\Http\Requests\StoreRegistrationRequest;
+use App\Models\Department;
 
 class RegistrationController
 {
@@ -22,10 +17,11 @@ class RegistrationController
         return Inertia::render('registrations/create', [
             'faculties' => Faculty::query()->whereRelation('universities', 'users.id', auth()->id())->get(['id', 'name']),
             'institutes' => Institute::query()->whereRelation('universities', 'users.id', auth()->id())->get(['id', 'name']),
+            'Departments' => Department::query()->whereRelation('universities', 'users.id', auth()->id())->get(['id', 'name']),
         ]);
     }
 
-    public function store(Student $student, StoreRegistrationRequest $storeRegistrationRequest) //: \Illuminate\Http\RedirectResponse
+    public function store(Student $student, StoreRegistrationRequest $storeRegistrationRequest): \Illuminate\Http\RedirectResponse
     {
         $storeRegistrationRequest->verify($student->latestRegistration);
         return redirect(RouteServiceProvider::HOME)->with('success', 'Student registered with success');
