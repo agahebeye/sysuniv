dac0425936cfbe0b164809addf03ea62aaff672f
agahebeye <agahebeye@gmail.com>
2022-06-10 17:59:55 +0200

    wip

--- routes/web/dashboard.php
+++ routes/web/dashboard.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Notifications\UniversityRegistered;
 use App\Http\Controllers\DashboardController;
 
-Route::permanentRedirect('/', '/dashboard');
-
 Route::middleware(['auth', 'role:admin,employee,university'])
-    ->get('/dashboard', [DashboardController::class, '__invoke']);
+    ->get('/', [DashboardController::class, '__invoke']);
+
+Route::get('/notification', function () {
+    $university = User::university()->latest()->first();
+    return (new UniversityRegistered(password: 'password'))
+        ->toMail($university);
+});
