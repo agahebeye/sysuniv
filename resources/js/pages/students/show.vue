259dd613c542f863445239eea327972031a1ee61
agahebeye <agahebeye@gmail.com>
2022-06-10 16:07:23 +0200

    wip

--- resources/js/pages/students/show.vue
+++ resources/js/pages/students/show.vue
@@ -2,7 +2,7 @@
 import { Head, Link } from '@inertiajs/inertia-vue3';
 import { useAuth } from '~/composables/auth';
 import { User } from '~/types/users';
-const { isAdmin, isEmployee, isUniversity } = useAuth();
+const { isAdmin, isEmployee } = useAuth();
 
 const props = defineProps<{
     student: any
@@ -16,6 +16,10 @@ const props = defineProps<{
         <Head>
             <title>Students/{{ student.firstname }} - Sysuniv</title>
         </Head>
+
+        <p v-if="isAdmin || isEmployee">
+            <Link :href="`/students/${student.id}/edit`">Edit this student</Link>
+        </p>
 
         <h1>
             <Link href="/students">Students</Link> / {{ student.firstname }} {{ student.lastname }}
