d7f74b54f478b09889cd6dfcc89ff7d89e3475bc
agahebeye <agahebeye@gmail.com>
2022-06-16 16:42:03 +0200

    wip

--- resources/js/pages/students/index.vue
+++ resources/js/pages/students/index.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { Inertia } from '@inertiajs/inertia';
 import { Head, Link } from '@inertiajs/inertia-vue3';
 import { useAuth } from '~/composables/auth';
 const { isAdmin, isEmployee, isUniversity } = useAuth();
@@ -6,6 +7,10 @@ const { isAdmin, isEmployee, isUniversity } = useAuth();
 const props = defineProps<{
     students: Array<any>
 }>();
+
+function takeme(url: string) {
+    Inertia.visit(url);
+}
 
 </script>
 
@@ -24,12 +29,24 @@ const props = defineProps<{
         <h2><strong>{{ students.length }}</strong> students</h2>
 
         <table>
-            <tr v-for="student in students" data-test="student">
-                <Link :href="`/students/${student.id}`">
-                <td>{{ student.firstname }}</td>
-                <td>{{ student.lastname }}</td>
-                </Link>
-            </tr>
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="pl-2 py-2">
+                        firstname
+                    </th>
+                    <th scope="col" class="px-4 py-2">
+                        lastname
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="student in students" class="cursor-pointer hover:bg-gray-100" @click="takeme(`/students/${student.id}`)" data-test="student">
+                    <td class="pl-2 py-1">{{ student.firstname }}</td>
+                    <td class="pl-2 py-1">{{ student.lastname }}</td>
+                </tr>
+            </tbody>
+
         </table>
     </div>
 </template>
