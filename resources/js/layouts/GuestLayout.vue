43a67c5b70b009917d2d6bfc544e8721896f5cfa
agahebeye <agahebeye@gmail.com>
2022-06-19 11:10:48 +0200

    wip

--- resources/js/layouts/GuestLayout.vue
+++ resources/js/layouts/GuestLayout.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex h-[100vh] w-full">
-        <header class="bg-gradient-to-b w-96 from-teal-900 to-white flex items-center">
-             <div class="px-10 text-sm space-y-3 text-white">
+        <header class="w-96 bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 min-h-screen flex items-center">
+             <div class="px-10 text-sm space-y-3">
                 <div>Republique du Burundi</div>
                 <div>Ministère de l'éducation nationale et de la recherche scientifique</div>
             </div>
