46594713b24931c1c47edc777b6004c50bd67276
agahebeye <agahebeye@gmail.com>
2022-06-23 09:13:14 +0200

    wip

--- resources/js/pages/employees/index.vue
+++ resources/js/pages/employees/index.vue
@@ -18,16 +18,19 @@ defineProps<{
             <title>Employees - Sysuniv</title>
         </Head>
 
-        <h1>Employees</h1>
+        <h1>Employées</h1>
 
-        <Link v-if="isAdmin || isEmployee" href="/employees/create" class="link">Create new employee</Link>
+        <Link v-if="isAdmin || isEmployee" href="/employees/create" class="link">Créer une nouvelle employée</Link>
 
-        <h2 v-if="employees.length"><strong>{{ employees.length }}</strong> employees</h2>
-        <h2 v-else>No employees already registered</h2>
+        <h2 class="mt-10 mb-8" v-if="employees.length"><strong>{{ employees.length }}</strong> employée(s)</h2>
+        <h2 class="mt-10 mb-8" v-else>Aucune employée enregistrée à la base</h2>
 
-        <div class="columns-2 gap-10 max-w-md">
-            <div v-for="employee in employees" class="mb-2" data-test="employee">
-                {{ employee.name }}
+        <div class="columns-3 gap-12 max-w-xl">
+            <div v-for="employee in employees" class="mb-4 flex items-center space-x-3" data-test="employee">
+                <img class="w-8 max-w-full rounded-full" src="/storage/avatars/avatar-placeholder.jpeg" :alt="`${employee.name} profile avatar`" />
+                <div class="text-base">
+                    {{ employee.name }}
+                </div>
             </div>
         </div>
     </div>
