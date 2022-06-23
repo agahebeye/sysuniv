342a6dfdc65c0b7f06ea8d876e966bb573cdbf7e
agahebeye <agahebeye@gmail.com>
2022-06-23 11:53:22 +0200

    wip

--- resources/js/pages/students/components/StudentList.vue
+++ resources/js/pages/students/components/StudentList.vue
@@ -5,22 +5,30 @@ defineProps({
 </script>
 
 <template>
-    <table class="max-w-md">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
+    <table class="w-[540px] table-fixed">
+        <thead class="text-xs text-gray-700 uppercase bg-black dark:bg-gray-700 dark:text-gray-400">
+            <tr class="text-left text-white">
                 <th scope="col" class="pl-2 py-2">
-                    firstname
+                   nom 
                 </th>
-                <th scope="col" class="px-4 py-2">
-                    lastname
+                <th scope="col" class="pl-2 py-2">
+                   prénom 
+                </th>
+                <th scope="col" class="pl-2 py-2">
+                   sexe
+                </th>
+                <th scope="col" class="pl-2 py-2">
+                   Année de naissance
                 </th>
             </tr>
         </thead>
 
         <tbody>
-            <tr v-for="student in students" class="cursor-pointer hover:bg-gray-100" @click="takeme(`/students/${student.id}`)" data-test="student">
-                <td class="pl-2 py-1">{{ student.firstname }}</td>
-                <td class="pl-2 py-1">{{ student.lastname }}</td>
+            <tr v-for="student in students" class="cursor-pointer hover:bg-gray-100 text-sm odd:bg-white even:bg-gray-100" @click="takeme(`/students/${student.id}`)" data-test="student">
+                <td class="pl-2 py-2">{{ student.firstname }}</td>
+                <td class="pl-2 py-2">{{ student.lastname }}</td>
+                <td class="pl-2 py-2">{{ student.gender }}</td>
+                <td class="pl-2 py-2">{{ student.birthDate }}</td>
             </tr>
         </tbody>
 
--- resources/js/pages/students/index.vue
+++ resources/js/pages/students/index.vue
@@ -22,15 +22,15 @@ function takeme(url: string) {
             <title>Students - Sysuniv</title>
         </Head>
 
-        <h1>Students</h1>
+        <h1>Etudiants</h1>
 
-        <Link v-if="isAdmin || isEmployee" href="/students/create" class="link">Register new student</Link>
-        <Link v-if="isUniversity" href="/registrations/create" class="link">Enroll new student</Link>
+        <Link v-if="isAdmin || isEmployee" href="/students/create" class="link">Enregistrer un nouveau étudiant</Link>
+        <Link v-if="isUniversity" href="/registrations/create" class="link">Inscrire un nouveau étudiant</Link>
 
-        <h2 v-if="students.length" class="!mb-0">{{ students.meta.from }} to {{ students.meta.to }} of {{ students.meta.total }} student(s)</h2>
-        <h2 v-else>No students already registered</h2>
+        <h2 class="mt-10 mb-8" v-if="students.data.length"><strong>{{ students.data.length }}</strong> étudiant(s)</h2>
+        <h2 class="mt-10 mb-8" v-else>Aucun étudiant a été enregistré</h2>
 
-        <div class="pagination" v-if="students.data.length > 1">
+        <div class="pagination mb-2" v-if="students.data.length > 1">
             <Component
                 :is="link.url ? Link : 'span'"
                 v-for="link in students.meta.links"
@@ -40,6 +40,6 @@ function takeme(url: string) {
                 :class="{ 'text-black': link.url, 'font-bold': link.active }" />
         </div>
 
-       <StudentList v-if="students.data.length" :students="students.data" />
+        <StudentList v-if="students.data.length" :students="students.data" />
     </div>
 </template>
