71e9d1543bb13920874980831947add6147c81b6
agahebeye <agahebeye@gmail.com>
2022-07-13 17:49:25 +0200

    wip

---
+++ database/seeders/InstituteSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Institute;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class InstituteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        collect([
+            'Institut de Statistique Appliquée',
+            'Institut d’administration et de cartographie foncières',
+            'Institut  de Pédagogie Appliquée',
+            'Institut Supérieur des Sciences Appliquées',
+            'Institut Supérieur de Formation Agricole',
+            'Institut Supérieur de Commerce',
+            'Institut d’Education Physique et des Sports',
+        ])->each(fn ($name) => Institute::factory()->create(['name' => $name]));
+    }
+}
