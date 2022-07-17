48fc8fc18db76baa6582a64e3d07d4ca2412ca90
agahebeye <agahebeye@gmail.com>
2022-07-17 12:34:40 +0200

    wip

--- resources/js/layouts/default.vue
+++ resources/js/layouts/default.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
+import { computed, ref } from 'vue';
 import { Link } from '@inertiajs/inertia-vue3';
 import LinkList from '~/components/shared/LinkList.vue';
 import SuccessAlert from '~/components/flash/SuccessAlert.vue';
 import AppHeader from '~/components/shared/AppHeader.vue';
 import {XIcon, MenuAlt1Icon} from '@heroicons/vue/outline';
-import { ref } from 'vue';
+
+import {useAuth} from '~/composables/auth';
+const homeLink = computed(() => useAuth().isUniversity.value ? '/registrations/create': '/' )
 
 const isMenuHidden = ref(true);
 
@@ -35,7 +38,7 @@ const isMenuHidden = ref(true);
             </nav>
 
             <main class="">
-                <Link href="/" class="inline-flex items-center mb-4 text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                <Link :href="homeLink" class="inline-flex items-center mb-4 text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
                 <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
                 </svg>
