f71091bfee8cd29843191eb13ea723e83b36ce00
agahebeye <agahebeye@gmail.com>
2022-07-21 08:00:08 +0200

    create StudentListTitle component

---
+++ resources/js/pages/students/components/StudentListTitle.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+    meta: Object
+})
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+const levelList = ['BAC I', 'BAC II', 'BAC III'];
+
+let temp = '';
+if (params['filter[gender]']) {
+    temp += `${params['filter[gender]']} `.toLowerCase()
+}
+
+if (params['filter[results.mention]']) {
+    temp += `${params['filter[results.mention]']} `.toLowerCase()
+}
+
+if (params['filter[freshmen]']) {
+    temp += `inscrits cette année `
+}
+
+if (params['filter[registrations.start_date]'] && params['filter[registrations.end_date]']) {
+    temp += `inscrits de ${params['filter[registrations.start_date]'] + 'à' + params['filter[registrations.end_date]']} `
+}
+
+if (params['filter[registrations.level]']) {
+    temp += `en ${levelList[params['filter[registrations.level]']]} `
+}
+
+if (params['filter[universities.name]']) {
+    temp += `à ${params['filter[universities.name]']} `
+}
+
+console.log(temp);
+
+</script>
+
+<template>
+    <h2 v-if="meta.total > 15">De {{ meta.from }} à {{ meta.to }} sur {{ meta.total }} étudiants</h2>
+</template>
--- resources/js/pages/students/index.vue
+++ resources/js/pages/students/index.vue
@@ -2,10 +2,11 @@
 import { Head, Link } from '@inertiajs/inertia-vue3';
 import DefaultLayout from '~/layouts/default.vue';
 import { useAuth } from '~/composables/auth';
+
 import StudentList from './components/StudentList.vue';
 import StudentPagination from './components/StudentPagination.vue';
 import TableFilters from './components/TableFilters.vue';
-import { computed } from 'vue';
+import StudentListTitle from './components/StudentListTitle.vue';
 
 const { isUniversity } = useAuth();
 
@@ -13,36 +14,6 @@ defineProps({
     students: Object,
 });
 
-const urlSearchParams = new URLSearchParams(window.location.search);
-const params = Object.fromEntries(urlSearchParams.entries());
-const levelList = ['BAC I', 'BAC II', 'BAC III'];
-
-let temp = '';
-if (params['filter[gender]']) {
-    temp += `${params['filter[gender]']} `.toLowerCase()
-}
-
-if (params['filter[results.mention]']) {
-    temp += `${params['filter[results.mention]']} `.toLowerCase()
-}
-
-if (params['filter[freshmen]']) {
-    temp += `inscrits cette année `
-}
-
-if (params['filter[registrations.start_date]'] && params['filter[registrations.end_date]']) {
-    temp += `inscrits de ${params['filter[registrations.start_date]'] + 'à' + params['filter[registrations.end_date]']} `
-}
-
-if (params['filter[registrations.level]']) {
-    temp += `en ${levelList[params['filter[registrations.level]']]} `
-}
-
-if (params['filter[universities.name]']) {
-    temp += `à ${params['filter[universities.name]']} `
-}
-
-console.log(temp);
 
 </script>
 
@@ -60,12 +31,9 @@ console.log(temp);
             <Link v-else href="/students/create" class="link">Enregistrer un nouveau étudiant</Link>
 
             <div v-if="students.data.length" class="relative flex flex-col mt-10 mb-8">
-                <h2 v-if="students.meta.total > 15">De {{ students.meta.from }} à {{ students.meta.to }} sur {{ students.meta.total }} étudiants</h2>
-                <h2 class="" v-else><strong>{{ students.data.length }}</strong> étudiant(s)</h2>
+                <StudentListTitle :meta="students.meta" />
                 <TableFilters />
             </div>
-
-            <h2 class="mt-10 mb-8" v-if="students.data.length === 0">{{ `${isUniversity ? 'Aucun étudiant a été inscrit' : 'Aucun étudiant a été enregistré'}` }}</h2>
 
             <StudentList v-if="students.data.length" :students="students.data" />
 
